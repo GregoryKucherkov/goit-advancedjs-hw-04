f78e7131a9dca8de092f0f9eaa6a051d60d453cb
Gregory Kucherkov <gregorykucherkov@Gregorys-Laptop.N4>
2024-07-28 14:24:22 +0300

    ver_2, updated promice creation for 2nd task

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -11,17 +11,46 @@ const onSubmitHandle = event => {
   const delay = parseInt(event.target.elements.delay.value, 10);
   const choice = event.target.elements.state.value;
 
-  const promise = new Promise((resolve, reject) => {
-    setTimeout(() => {
-      if (choice === 'fulfilled') {
-        resolve(delay);
-      } else {
-        reject(delay);
-      }
-    }, delay);
-  });
+  // const promise = new Promise((resolve, reject) => {
+  //   setTimeout(() => {
+  //     if (choice === 'fulfilled') {
+  //       resolve(delay);
+  //     } else {
+  //       reject(delay);
+  //     }
+  //   }, delay);
+  // });
 
-  promise
+  // promise
+  //   .then(value => {
+  //     iziToast.success({
+  //       message: `✅ Fulfilled promise in ${value}ms`,
+  //       messageColor: '#FFF',
+  //       backgroundColor: '#00c74b',
+  //       messageSize: '24',
+  //       position: 'topRight',
+  //       icon: '',
+  //       timeout: 4000,
+  //     });
+  //   })
+  //   .catch(error => {
+  //     iziToast.error({
+  //       message: `❌ Rejected promise in ${error}ms`,
+  //       messageColor: '#FFF',
+  //       backgroundColor: '#FF6865',
+  //       messageSize: '24',
+  //       position: 'topRight',
+  //       icon: '',
+  //       timeout: 4000,
+  //     });
+  //   });
+
+  // Alternative promise code
+  new Promise((resolve, reject) => {
+    setTimeout(() => {
+      choice === 'fulfilled' ? resolve(delay) : reject(delay);
+    }, delay);
+  })
     .then(value => {
       iziToast.success({
         message: `✅ Fulfilled promise in ${value}ms`,
@@ -44,6 +73,7 @@ const onSubmitHandle = event => {
         timeout: 4000,
       });
     });
+  
 };
 
 form.addEventListener('submit', onSubmitHandle);
